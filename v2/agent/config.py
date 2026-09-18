b075c11aeb52e7f84a823eb2a2620ac1f9a12e83
Eung-Seok <rwjflrk1234@gmail.com>
2026-09-18 06:25:35 -0700

    config: externalize agent runtime settings

--- v2/agent/config.py
+++ v2/agent/config.py
@@ -1,9 +1,10 @@
-# 클라우드 배포 후 Railway URL로 이 줄만 교체하면 됨
-# API_URL = "http://localhost" # 개발환경
-API_URL = "http://a8696e16291a949eabfb2456ef0db4df-212562611.ap-northeast-2.elb.amazonaws.com" # k8s 실제 주소.
+import os
+
+
+API_URL = os.getenv("HPDFS_API_URL", "http://localhost").rstrip("/")
 
 # 수집 간격 (분)
-INTERVAL_MINUTES = 5
+INTERVAL_MINUTES = int(os.getenv("HPDFS_INTERVAL_MINUTES", "5"))
 
 # 요청 타임아웃 (초)
-REQUEST_TIMEOUT = 10
+REQUEST_TIMEOUT = int(os.getenv("HPDFS_REQUEST_TIMEOUT", "10"))
